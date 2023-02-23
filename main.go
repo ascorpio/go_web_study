3bc2d787784874a11655dc726bdb259504ff70c1
wjh <ascorpio@qq.com>
2023-02-23 22:35:11 +0800

    设置标头

--- main.go
+++ main.go
@@ -6,19 +6,20 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	switch r.URL.Path {
 	case "/":
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog 啊哈哈哈哈</h1>")
+		_, _ = fmt.Fprint(w, "<h1>Hello, 这里是 goblog 啊哈哈哈哈</h1>")
 	case "/about":
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		_, _ = fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	default:
-		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+		_, _ = fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
 			"<p>如有疑惑，请联系我们。</p>")
 	}
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	_ = http.ListenAndServe(":3000", nil)
 }
