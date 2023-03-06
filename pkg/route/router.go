434b312b8e0157fd6139775deb49e415d6cd5d7a
wjh <ascorpio@qq.com>
2023-03-06 22:09:54 +0800

    重构至 PagesController

--- pkg/route/router.go
+++ pkg/route/router.go
@@ -3,6 +3,7 @@ package route
 import (
 	"github.com/gorilla/mux"
 	"goblog/pkg/logger"
+	"goblog/routes"
 	"net/http"
 )
 
@@ -12,6 +13,7 @@ var Router *mux.Router
 // Initialize 初始化路由
 func Initialize() {
 	Router = mux.NewRouter()
+	routes.RegisterWebRoutes(Router)
 }
 
 // RouteName2URL 通过路由名称来获取 URL
