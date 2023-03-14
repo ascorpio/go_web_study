8008a04ea465a182b38091833b017876b28d8e4d
wjh <wujinhong@isaveu.cn>
2023-03-14 11:40:30 +0800

    登录表单和会话控制

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -6,9 +6,9 @@ import "goblog/app/models"
 type User struct {
 	models.BaseModel
 
-	Name     string `gorm:"type:varchar(255);not null;unique" valid:"name"`
-	Email    string `gorm:"type:varchar(255);unique;" valid:"email"`
-	Password string `gorm:"type:varchar(255)" valid:"password"`
+	Name     string `gorm:"type:varchar(100);not null;unique" valid:"name"`
+	Email    string `gorm:"type:varchar(100);unique;" valid:"email"`
+	Password string `gorm:"type:varchar(100)" valid:"password"`
 
 	// gorm:"-" —— 设置 GORM 在读写时略过此字段，仅用于表单验证
 	PasswordConfirm string `gorm:"-" valid:"password_confirm"`
